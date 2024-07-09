1fdb40db4599c20c6c683cd14474cbd44764c8a0
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2024-07-09 17:54:27 +0000

    updated task 10

---
+++ Javascript_advanced/10-prime.js
@@ -0,0 +1,33 @@
+function countPrimeNumbers() {
+  function isPrime(num) {
+    if (num <= 1) return false;
+    if (num <= 3) return true;
+    if (num % 2 === 0 || num % 3 === 0) return false;
+    for (let i = 5; i * i <= num; i += 6) {
+      if (num % i === 0 || num % (i + 2) === 0) return false;
+    }
+    return true;
+  }
+
+  let primeCount = 0;
+  for (let i = 2; i <= 100; i++) {
+    if (isPrime(i)) {
+      primeCount++;
+    }
+  }
+  return primeCount;
+}
+
+const start = performance.now();
+
+for (let i = 0; i < 100; i++) {
+  countPrimeNumbers();
+}
+
+const end = performance.now();
+
+console.log(
+  `Execution time of calculating prime numbers 100 times was ${
+    end - start
+  } milliseconds.`
+);
