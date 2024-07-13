8f1fd512c617f31af5d0e15fcdaedb96928e8c4d
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2024-07-13 15:12:18 +0000

    added cookies proj files

---
+++ Cookies_local_storage/webpack.config.js
@@ -0,0 +1,14 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  devServer: {
+    static: path.resolve(__dirname, "dist"),
+    compress: true,
+    port: 9001, // Changed port number
+  },
+};
